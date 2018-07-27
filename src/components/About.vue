400a791fed53823bbba738b7295715d25364a57a
Christopher Brown <chbphone55@gmail.com>
2018-07-26 23:18:33 -0600

    basic server setup with vue-router

---
+++ src/components/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <h1>Welcome to the about section.</h1>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <h1>Welcome to the homepage.</h1>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
